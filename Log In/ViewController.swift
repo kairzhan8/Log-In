343ebdab54e00c47799eb8e70675445daca81184
kairzhan8 <kairzhankural@gmail.com>
2020-08-10 12:32:19 +0600

    Initial Commit

---
+++ Log In/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Log In
+//
+//  Created by Kairzhan Kural on 8/10/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
